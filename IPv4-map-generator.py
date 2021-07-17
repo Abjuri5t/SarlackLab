2a3698f0fc579cf5c40e00d4680335f9b8705544
Abjuri5t <user_pi@raspberrypi>
2021-07-17 22:43:57 +0100

    removed double-counting of malware campaign directories

--- IPv4-map-generator.py
+++ IPv4-map-generator.py
@@ -11,7 +11,7 @@ timeStr = ""
 def main():
     if(len(sys.argv) != 3):
         print("incorrect number of arguments given")
-        sys.exit()
+        print("Using \"command-and-control-servers.list\" and \"malware-payload-urls.list\" as default")
 
     ccServ = open(sys.argv[1], 'r')
     badIPs = ccServ.read().splitlines()
@@ -20,9 +20,9 @@ def main():
 
     #sort and filter
     print("\nStarting recursive IP insertion sort:")
-    badIPs = ip_insertionSort(badIPs, 3)
+    badIPs = ip_lambdaSort(badIPs)
     top10Total = getTop10D1(badIPs)
-    malIPs = ip_insertionSort(malIPs, 3)
+    malIPs = ip_lambdaSort(badIPs)
 
     #find most dangerous IP/8 and IP
     worst8 = [[]]
@@ -33,11 +33,47 @@ def main():
     for bad in worst8:
         if((not(isinstance(bad[0], int))) and (bad[1] != 0)):
             print(bad[0]+".0.0.0/8 hosts "+str(bad[1]))
-	
     now = datetime.now()
     global timeStr
     timeStr = str(now.strftime("%Y-%B-%d_%H:%M"))
+    dispSetup(top10Worst8, top10Total)
     pygameHilbert(worst8, mal8)
+
+
+def dispSetup(worst8, topIPs):
+    with open("template.html.dontdel", 'r') as template:
+        htmlSrc = template.read()
+    template.close()
+    fileName = timeStr+".html"
+    with open(fileName, 'w') as outPage:
+        rplStr = htmlSrc.replace("screenshot", timeStr)
+        rplStr = rplStr.replace("rplc0rng", worst8[0][0])
+        rplStr = rplStr.replace("rplc1rng", worst8[1][0])
+        rplStr = rplStr.replace("rplc2rng", worst8[2][0])
+        rplStr = rplStr.replace("rplc3rng", worst8[3][0])
+        rplStr = rplStr.replace("rplc4rng", worst8[4][0])
+        rplStr = rplStr.replace("rplc5rng", worst8[5][0])
+        rplStr = rplStr.replace("rplc6rng", worst8[6][0])
+        rplStr = rplStr.replace("rplc7rng", worst8[7][0])
+        rplStr = rplStr.replace("rplc8rng", worst8[8][0])
+        rplStr = rplStr.replace("rplc9rng", worst8[9][0])
+#        ipStrs = getIPStrs(topIPs)
+        rplStr = rplStr.replace("rplc0ip", topIPs[0][0])
+        rplStr = rplStr.replace("rplc1ip", topIPs[1][0])
+        rplStr = rplStr.replace("rplc2ip", topIPs[2][0])
+        rplStr = rplStr.replace("rplc3ip", topIPs[3][0])
+        rplStr = rplStr.replace("rplc4ip", topIPs[4][0])
+        rplStr = rplStr.replace("rplc5ip", topIPs[5][0])
+        rplStr = rplStr.replace("rplc6ip", topIPs[6][0])
+        rplStr = rplStr.replace("rplc7ip", topIPs[7][0])
+        rplStr = rplStr.replace("rplc8ip", topIPs[8][0])
+        rplStr = rplStr.replace("rplc9ip", topIPs[9][0])
+        outPage.write(rplStr)
+    outPage.close()
+    for space in worst8:
+        for ip in topIPs:
+            if(space[0] == ip[0].split('.')[0]):
+                print(ip[0])
 
 
 def getTop10D2(badItem):
@@ -87,18 +123,11 @@ def retOne(item):
     return item["one"]
 
 
-def ip_insertionSort(inputList, t):
-    for e in range(1, len(inputList)):
-        r = e - 1
-        nxtElement = inputList[e]
-        while(int(inputList[r].split('.')[t]) > int(nxtElement.split('.')[t])) and (r >= 0):
-            inputList[r+1] = inputList[r]
-            r = r - 1
-        inputList[r+1] = nxtElement
-    if(t == 0):
-        return inputList
-    else:
-        return ip_insertionSort(inputList, t-1)
+def ip_lambdaSort(inputList):
+    retList = []
+    for ip in sorted(inputList, key = lambda ip: [int(ip) for ip in ip.split('.')]):
+        retList.append(ip)
+    return retList
 
 
 def getSlash(ipList, deg):
@@ -118,6 +147,7 @@ def getSlash(ipList, deg):
 
 
 def pygameHilbert(worst, mal):
+    print("pygame may cause program to crash on some systems - work in progress")
     pygame.init()
     screen = pygame.display.set_mode((60*16, 60*16))
     rows = [[0, 1, 14, 15, 16, 19, 20, 21, 234, 235, 236, 239, 240, 241, 254, 255], [3, 2, 13, 12, 17, 18, 23, 22, 233, 232, 237, 238, 243, 242, 253, 252], [4, 7, 8, 11, 30, 29, 24, 25, 230, 231, 226, 225, 244, 247, 248, 251], [5, 6, 9, 10, 31, 28, 27, 26, 229, 228, 227, 224, 245, 246, 249, 250], [58, 57, 54, 53, 32, 35, 36, 37, 218, 219, 220, 223, 202, 201, 198, 197], [59, 56, 55, 52, 33, 34, 39, 38, 217, 216, 221, 222, 203, 200, 199, 196], [60, 61, 50, 51, 46, 45, 40, 41, 214, 215, 210, 209, 204, 205, 194, 195], [63, 62, 49, 48, 47, 44, 43, 42, 213, 212, 211, 208, 207, 206, 193, 192], [64, 67, 68, 69, 122, 123, 124, 127, 128, 131, 132, 133, 186, 187, 188, 191], [65, 66, 71, 70, 121, 120, 125, 126, 129, 130, 135, 134, 185, 184, 189, 190], [78, 77, 72, 73, 118, 119, 114, 113, 142, 141, 136, 137, 182, 183, 178, 177], [79, 76, 75, 74, 117, 116, 115, 112, 143, 140, 139, 138, 181, 180, 179, 176], [80, 81, 94, 95, 96, 97, 110, 111, 144, 145, 158, 159, 160, 161, 174, 175], [83, 82, 93, 92, 99, 98, 109, 108, 147, 146, 157, 156, 163, 162, 173, 172], [84, 87, 88, 91, 100, 103, 104, 107, 148, 151, 152, 155, 164, 167, 168, 171], [85, 86, 89, 90, 101, 102, 105, 106, 149, 150, 153, 154, 165, 166, 169, 170]]#This is not efficient programming, I am well aware. I tried the prettier algorithms but I gave up after trying and failing too much
@@ -138,8 +168,8 @@ def draw16x16(boxSize, space, color, screen):
 
 
 def fill16x16(boxSize, worst, mal, screen, rows):
-    c2Weight = 4
-    malWeight = 1
+    c2Weight = 6 #try to keep x4 larger than malWeight
+    malWeight = 1.5
     worst = populate(worst)
     mal = populate(mal)
     masterCnt = 0
@@ -199,6 +229,22 @@ def grabBad(wantedVal, array):
     for i in array:
         if(i[0] == wantedVal):
             return i
+    print("That's an error in grabBad(), bub")
+
+
+"""def getIPStrs(ipList):!!!
+    retStrs = []
+    for ipStruct in ipList:
+        cmd = "whois "+ipStruct[0]+" | grep netname"
+        response = os.popen(cmd).read()
+        parse = response.split(' ')
+        netName = parse[len(parse) - 1]
+        netName = netName.strip()
+        if(netName != ''):
+            retStrs.append(ipStruct[0]+" ("+netName+')')
+        else:
+            retStrs.append(ipStruct[0])
+    return retStrs"""
 
 
 
